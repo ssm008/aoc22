3707c63d05751bb63c74a64817c2207b014484a6
Simon Smeds <simon.smeds@wapice.com>
2023-01-19 13:10:24 +0200

    day19 wip

---
+++ src/day19.c
@@ -0,0 +1,37 @@
+#include "aocutils.h"
+
+int part1(char *buf, int bufsize) { return 0; }
+
+int part2(char *buf, int bufsize) { return 0; }
+
+int main(int argc, char *argv[]) {
+  clock_t timer = clock();
+  FILE *inputFile = fopen(argv[1], "r");
+  int bufsize = 0;
+
+  fseek(inputFile, 0, SEEK_END);
+  bufsize = ftell(inputFile);
+  rewind(inputFile);
+
+  char buf[20000];
+  fread(buf, 1, bufsize, inputFile);
+  buf[bufsize] = 0;
+  fclose(inputFile);
+
+  printf("Day 19\n");
+  timer = clock() - timer;
+  printf("\tBuffering input file: %d bytes in %d µs\n", bufsize,
+         (int)((double)timer / CLOCKS_PER_SEC * 1000000));
+
+  timer = clock();
+  int part1_solution = part1(buf, bufsize);
+  timer = clock() - timer;
+  printf("\tPart 1 solution: %d (%d µs)\n", part1_solution,
+         (int)((double)timer / CLOCKS_PER_SEC * 1000000));
+
+  timer = clock();
+  int part2_solution = part2(buf, bufsize);
+  timer = clock() - timer;
+  printf("\tPart 2 solution: %d (%d µs)\n", part2_solution,
+         (int)((double)timer / CLOCKS_PER_SEC * 1000000));
+}
